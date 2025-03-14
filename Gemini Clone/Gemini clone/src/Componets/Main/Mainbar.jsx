b3cef386af9e0b82213a7a865a4e8815ce307a59
Vikalp Nag <vikalpnag@gmail.com>
2025-03-14 16:39:26 +0530

    Message and Card ui

--- Gemini Clone/Gemini clone/src/Componets/Main/Mainbar.jsx
+++ Gemini Clone/Gemini clone/src/Componets/Main/Mainbar.jsx
@@ -9,6 +9,32 @@ const Mainbar = () => {
         <p>Gemini</p>
         <img src={assets.user_icon} alt="" />
       </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello Vikalp</span>
+          </p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest some places?</p>
+            <img src={assets.compass_icon} alt="question" />
+          </div>
+          <div className="card">
+            <p>Summarize about poverty</p>
+            <img src={assets.bulb_icon} alt="question" />
+          </div>
+          <div className="card">
+            <p>What is the capital of India?</p>
+            <img src={assets.message_icon} alt="question" />
+          </div>
+          <div className="card">
+            <p>What is React.js</p>
+            <img src={assets.code_icon} alt="question" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
