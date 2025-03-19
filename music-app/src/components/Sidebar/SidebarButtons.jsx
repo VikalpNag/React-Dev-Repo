7fbb4823e2ea3a5035f6ea5524ff4a89db6d52e3
Vikalp Nag <vikalpnag@gmail.com>
2025-03-19 12:32:01 +0530

    Active btn

--- music-app/src/components/Sidebar/SidebarButtons.jsx
+++ music-app/src/components/Sidebar/SidebarButtons.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./SidebarButtons.css";
+import { IconContext } from "react-icons";
 
-const SidebarButtons = () => {
-  return <div>S</div>;
+const SidebarButtons = (props) => {
+  const location = useLocation();
+  const isActive = location.pathname === props.to;
+  const btnClass = isActive ? "btn-body active" : "btn-body";
+  return (
+    <Link to={props.to}>
+      <IconContext.Provider value={{ size: "24px", className: "btn-icon" }}>
+        <div className={btnClass}>
+          {props.icon}
+          <p className="btn-title">{props.title}</p>
+        </div>
+      </IconContext.Provider>
+    </Link>
+  );
 };
 
 export default SidebarButtons;
