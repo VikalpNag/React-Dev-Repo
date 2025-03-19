ee26dd78c136e153e167f13b4413921cd3a2c48f
Vikalp Nag <vikalpnag@gmail.com>
2025-03-19 23:01:05 +0530

    playlist fetching

--- music-app/src/screens/Library/Library.jsx
+++ music-app/src/screens/Library/Library.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import APIKit from "../../spotify";
+import "./library.css";
 
 const Library = () => {
-  return <div className="screen-container"></div>;
+  const [playlists, setPlaylists] = useState(null);
+
+  useEffect(() => {
+    APIKit.get("me/playlists").then(function (response) {
+      setPlaylists(response.data.items);
+      console.log(response.data.items);
+    });
+  }, []);
+
+  return (
+    <div className="screen-container">
+      <div className="library-body">
+        {playlists?.map((playlist) => (
+          <div>{playlist.name}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Library;
