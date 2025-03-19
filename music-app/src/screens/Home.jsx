e2cefd03aef267878b1655bdbf0e845ff32451d3
Vikalp Nag <vikalpnag@gmail.com>
2025-03-19 07:57:43 +0530

    Router setup

--- music-app/src/screens/Home.jsx
+++ music-app/src/screens/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Library from "./Library";
+import Feed from "./Feed";
+import Favorites from "./Favorites";
+import Player from "./Player";
+import Trending from "./Trending";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Library />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/player" element={<Player />} />
+        <Route path="/trending" element={<Trending />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default Home;
