0a5894e66c0b6c78125cd5de6ac5fafc7d1a0e0f
Vikalp Nag <vikalpnag@gmail.com>
2025-03-20 23:31:59 +0530

    navigate on play btn

--- music-app/src/screens/Library/Library.jsx
+++ music-app/src/screens/Library/Library.jsx
@@ -3,6 +3,7 @@ import APIKit from "../../spotify";
 import "./library.css";
 import { IconContext } from "react-icons";
 import { AiFillPlayCircle } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 const Library = () => {
   const [playlists, setPlaylists] = useState(null);
@@ -13,12 +14,21 @@ const Library = () => {
       console.log(response.data.items);
     });
   }, []);
+  const navigate = useNavigate();
+
+  const playPlayList = (id) => {
+    navigate("/player", { state: { id: id } });
+  };
 
   return (
     <div className="screen-container">
       <div className="library-body">
         {playlists?.map((playlist) => (
-          <div className="playlist-card">
+          <div
+            className="playlist-card"
+            key={playlist?.id}
+            onClick={() => playPlayList(playlist.id)}
+          >
             <img
               src={playlist.images[0].url}
               className="playlist-image"
@@ -27,7 +37,7 @@ const Library = () => {
             <p className="playlist-title">{playlist.name.substring(0, 18)}</p>
             <p className="playlist-subtitle">{playlist.tracks.total} Songs</p>
             <div className="playlist-fade">
-              <IconContext.Provider value={{ size: "50px", color: "#E99D72" }}>
+              <IconContext.Provider value={{ size: "40px", color: "#E99D72" }}>
                 <AiFillPlayCircle />
               </IconContext.Provider>
             </div>
