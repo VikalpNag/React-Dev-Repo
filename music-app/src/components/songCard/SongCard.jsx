f017380a198727c76bf8b1c7bacac545af377b74
Vikalp Nag <vikalpnag@gmail.com>
2025-03-21 18:56:17 +0530

    Song Card

--- music-app/src/components/songCard/SongCard.jsx
+++ music-app/src/components/songCard/SongCard.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./songCard.css";
 
 const SongCard = () => {
-  return <div>SongCard</div>;
+  return (
+    <div className="songCard-body">
+      <AlbumImage />
+      <AlbumInfo />
+    </div>
+  );
 };
 
 export default SongCard;
