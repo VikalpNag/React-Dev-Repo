4274e14dfa81e4f7b89372e24d03a9f771c5db3c
Vikalp Nag <vikalpnag@gmail.com>
2025-03-22 10:56:09 +0530

    Album image styling on Player

--- music-app/src/components/songCard/AlbumImage.jsx
+++ music-app/src/components/songCard/AlbumImage.jsx
@@ -4,10 +4,8 @@ import "./albumImage.css";
 const AlbumImage = (url) => {
   console.log(url?.url);
   return (
-    <div>
-      <div className="albumImage">
-        <img src={url?.url} alt="album art" className="albumImage-art" />
-      </div>
+    <div className="albumImage flex">
+      <img src={url?.url} alt="album art" className="albumImage-art" />
       <div className="albumImage-shadow">
         <img src={url?.url} alt="shadow" className="albumImage-shadow" />
       </div>
