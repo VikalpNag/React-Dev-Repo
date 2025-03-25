33ffd9071e6c5428b6428cc706b80374cf385895
Vikalp Nag <vikalpnag@gmail.com>
2025-03-25 22:35:53 +0530

    created widgets card

---
+++ music-app/src/components/Widgets/WidgetCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./widgetCard.css";
+
+const WidgetCard = () => {
+  return <div>WidgetCard</div>;
+};
+
+export default WidgetCard;
--- music-app/src/components/Widgets/Widgets.jsx
+++ music-app/src/components/Widgets/Widgets.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./widgets.css";
+import apiClient from "../../spotify.js";
+import WidgetCard from "./WidgetCard.jsx";
 
 const Widgets = ({ artistId }) => {
-  return <div></div>;
+  const [similar, setSimilar] = useState([]);
+  const [featured, setFeatured] = useState([]);
+  const [newRelease, setNewRelease] = useState([]);
+  const id = artistId?.artists[0]?.id;
+
+  useEffect(() => {
+    apiClient
+      .get(`/artists/${id}/related-artists`)
+      .then((res) => {
+        const a = res.data?.artists.slice(0, 3);
+        setSimilar(a);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    apiClient
+      .get(`/browse/featured-playlists`)
+      .then((res) => {
+        const a = res.data?.playlists.items.slice(0, 3);
+        setFeatured(a);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    apiClient
+      .get(`/browse/new-release`)
+      .then((res) => {
+        const a = res.data?.albums.items.slice(0, 3);
+        setNewRelease(a);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
+
+  return (
+    <div className="widgets-body">
+      <WidgetCard title="Similar Artists" similar={similar} />
+      <WidgetCard title="Made for you" similar={featured} />
+      <WidgetCard title="New Release" similar={newRelease} />
+    </div>
+  );
 };
 
 export default Widgets;
