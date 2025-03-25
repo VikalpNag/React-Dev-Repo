0a3f6270784997d68fe4ceab31450b140a5a1021
Vikalp Nag <vikalpnag@gmail.com>
2025-03-26 01:21:34 +0530

    #1

--- music-app/src/components/Widgets/WidgetEntry.jsx
+++ music-app/src/components/Widgets/WidgetEntry.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./widgetEntry.css";
 
-const WidgetEntry = () => {
-  return <div>WidgetEntry</div>;
+const WidgetEntry = ({ title, subtitle, image }) => {
+  return (
+    <div className="entry-body">
+      <img src={image} alt={title} className="entry-image" />
+      <div className="entry-right">
+        <p className="entry-title">{title}</p>
+      </div>
+    </div>
+  );
 };
 
 export default WidgetEntry;
