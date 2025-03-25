ad6c3c38374a93b76d910ee7bd48d2c07692a656
Vikalp Nag <vikalpnag@gmail.com>
2025-03-26 00:02:05 +0530

    added new components for widgets

--- music-app/src/components/Widgets/WidgetCard.jsx
+++ music-app/src/components/Widgets/WidgetCard.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import "./widgetCard.css";
+import WidgetEntry from "./WidgetEntry";
 
-const WidgetCard = () => {
-  return <div>WidgetCard</div>;
+const WidgetCard = ({ title, similar, featured, newRelease }) => {
+  return (
+    <div className="WidgetCard-body">
+      <p className="widget-title">{title}</p>
+      {similar
+        ? similar.map((artist) => (
+            <WidgetEntry
+              key={artist?.id}
+              title={artist?.name}
+              subtitle={artist?.follower?.total}
+              image={artist?.images?.[2]?.url}
+            />
+          ))
+        : featured
+        ? featured.map((playlist) => (
+            <WidgetEntry
+              key={playlist?.id}
+              title={playlist?.name}
+              subtitle={playlist?.tracks?.total}
+              image={playlist?.images?.[2]?.url}
+            />
+          ))
+        : newRelease?.map((album) => (
+            <WidgetEntry
+              key={album?.id}
+              title={album?.name}
+              subtitle={album?.artist?.[0]?.name}
+              image={album?.images?.[2]?.url}
+            />
+          ))}
+    </div>
+  );
 };
 
 export default WidgetCard;
---
+++ music-app/src/components/Widgets/WidgetEntry.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./widgetEntry.css";
+
+const WidgetEntry = () => {
+  return <div>WidgetEntry</div>;
+};
+
+export default WidgetEntry;
